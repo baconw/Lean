65cdcf3541e47400145e5878bfd078ef59b78e2f
baconw <peidong.wei@gmail.com>
2016-07-13 14:24:25 +0800

    Initial Commit

---
+++ Lean2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lean2
+//
+//  Created by dongyi228 on 16/7/13.
+//  Copyright (c) 2016年 dongyi228. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Lean2Tests/Lean2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Lean2Tests.swift
+//  Lean2Tests
+//
+//  Created by dongyi228 on 16/7/13.
+//  Copyright (c) 2016年 dongyi228. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Lean2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
